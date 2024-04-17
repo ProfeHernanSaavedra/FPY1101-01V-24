5b4a518a3178f7fd97035d1e49142f9831bc23cc
USRVI-LC5 <USRVI-LC5@duoc.acad>
2024-04-16 20:15:48 -0400

    mi primer python

---
+++ hola.py
@@ -0,0 +1,23 @@
+print("hola mundo")
+print("Mi primer programa")
+print("Ingrese su nombre")
+#print() # comentario de linea 
+nombre = input() # leer nombre
+
+num1 = input("Ingrese numero 1: ")
+num1Entero = int(num1)
+num2 = float(input("Ingrese numero 2: "))
+suma = num1Entero + num2
+print("La suma de ",num1," + ",num2," es ",suma)
+
+palabra = "hola"
+decimal = 3.9
+entero = 30
+verdad = False # o True Boolean
+
+##calcular edad
+print("Calculo de edad")
+añoActual = int(input("Ingrese año Actual: "))
+añoNac = int(input("Ingrese año Nacimiento: "))
+edad = añoActual - añoNac
+print("Su edad es: ",edad," app")
