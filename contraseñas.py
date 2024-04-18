bc0479681c35f4817b9b77df6075e5efbfdd6d80
ProfeHernanSaavedra <hern.saavedra@profesor.duoc.cl>
2024-04-17 21:51:10 -0400

    si anidados ejemplo passw

---
+++ contraseñas.py
@@ -0,0 +1,19 @@
+usuario1 = "pedro"
+usuario2 = "angel"
+pass1 = "1234"
+pass2 ="a4s1"
+
+user = input("Ingrese usuario: ")
+password  = input("Ingrese clave: ")
+
+if user == usuario1 and password == pass1:
+    print("Bienvenido ", user)
+else:
+    if user == usuario2 and password == pass2:
+        print("Bienvenido ", user)
+    else:
+        print("Usuario no válido")
+    
+
+
+ 
