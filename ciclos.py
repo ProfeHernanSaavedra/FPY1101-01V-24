9328c28803ffeba5ef238b1c1d6d17fa961bea1f
ProfeHernanSaavedra <hern.saavedra@profesor.duoc.cl>
2024-04-23 20:29:40 -0400

    ciclo for

---
+++ ciclos.py
@@ -0,0 +1,11 @@
+# for ---> para
+'''
+for num in range(3):
+    print(num)
+'''
+## tabla multiplicar del 1 al 10 de cualquier número 
+num = int(input("Ingrese el número: "))
+
+for i in range(1,11):
+    multi = num*i
+    print(num,"x",i,"=",multi)
