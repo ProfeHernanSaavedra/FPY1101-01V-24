bc972e9886cb2e60d4d6baf085928d908659be55
ProfeHernanSaavedra <hern.saavedra@profesor.duoc.cl>
2024-06-04 20:29:34 -0400

    funciones

---
+++ defFunciones.py
@@ -0,0 +1,24 @@
+'''
+funciones sin argumentos y sin retorno 1 OK
+funciones con argumentos y con retorno 4 OK
+funciones con argumentos y sin retorno 2 OK
+funciones sin argumentos y con retorno 3 OK
+
+'''
+
+#1 definir una funcion
+def saludo():
+    print("Hola")
+#2
+def saludoNombre(nombre):
+    print("Hola" , nombre)
+
+#3
+def suma():
+    suma = 3 + 3
+    return suma
+#4    
+def suma2(num1,num2):
+    suma = num1 + num2
+    return suma
+    
---
+++ funciones.py
@@ -0,0 +1,12 @@
+import defFunciones as fn
+
+#llamando funciones
+fn.saludo()
+fn.saludoNombre("JUACO")
+fn.saludoNombre("VANIA")
+
+print(fn.suma())
+num1 = int(input("Ingrese número 1: "))
+num2 = int(input("Ingrese número 2: "))
+print(fn.suma2(num1,num2))
+
